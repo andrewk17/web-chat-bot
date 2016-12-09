e2d51a7d45f7aad0736a6432fe6aa5304d91ca5d
Andrew <andrewk17@gmail.com>
2016-12-09 11:20:37 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# web-chat-bot
+Web Chat Bot
---
+++ client/app/app.js
@@ -0,0 +1,11 @@
+angular
+  .module('app', [
+  ])
+  .controller('ChatBoxController', [function () {
+
+  }])
+  .directive('chatBox', function() {
+    return {
+      template: '<p>Hello World</p>'
+    }
+  })
