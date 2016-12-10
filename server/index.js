8542252678660a545d25555263dc03faf9735d91
Andrew <andrewk17@gmail.com>
2016-12-10 13:04:50 -0800

    Testing CORS

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const bodyParser = require('body-parser');
 const dbController = require('./db/controller.js');
 const botResponses = require('./botResponses.js')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname + '/../client/app')));
