6d0189f72ed171daf7b6f4a581e38c166ee89c74
Andrew <andrewk17@gmail.com>
2016-12-10 13:21:16 -0800

    Remove mobile debugging items

--- server/index.js
+++ server/index.js
@@ -6,12 +6,6 @@ const bodyParser = require('body-parser');
 const dbController = require('./db/controller.js');
 const botResponses = require('./botResponses.js')
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname + '/../client/app')));
