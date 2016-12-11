a37ad4efef4a70df8a1e3fa63f93c97f69f197e3
Andrew <andrewk17@gmail.com>
2016-12-11 15:22:16 -0800

    Fixes punctation on robot question
    Updates readme

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # web-chat-bot
 Web Chat Bot
 
+Install dependencies
+
 `npm install`
 
+Build applicaiton
+
 `npm run build`
+
+Start server
 
 `npm start`
--- server/config.js
+++ server/config.js
@@ -15,7 +15,7 @@ module.exports = {
     name: 'What\'s your name?',
     address: 'What\'s your address?',
     education: 'Tell me about your education.',
-    workExperience: 'Tell me about your work experience',
+    workExperience: 'Tell me about your work experience.',
     resume: 'Where can I see your resume?',
     linkedIn: 'What\'s the link to your LinkedIn profile?'
   }
